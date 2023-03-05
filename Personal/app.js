2e5d333daacc3083f3fd96c068653037392ba11b
AlexGrimaldo <97560684+AlexGrimaldo@users.noreply.github.com>
2023-03-05 14:49:57 -0600

    Getting Close to Completion

--- Personal/app.js
+++ Personal/app.js
@@ -44,6 +44,43 @@ $('#btnContact').click(function(){
         },
         showConfirmButton: false,
         timerProgressBar: true,
-        html: 'Hello'
+        html: 
+            `
+            <h3 class="text-center">Alex Grimaldo</h3>
+            <h5><i class="bi bi-envelope-fill"></i> : <a href="mailto: grimaldoj53@gmail.com" aria-label="Email Address">grimaldoj53@gmail.com</a></h5>
+            `
+    })
+})
+
+$('#btnMenu').click(function(){
+    Swal.fire({
+        toast: true,
+        position: 'top',
+        showClass: {
+            popup: `
+              animate__animated
+              animate__fadeInDown
+              animate__faster
+            `
+        },
+        hideClass: {
+        popup: `
+            animate__animated
+            animate__fadeOutUp
+            animate__faster
+        `
+        },
+        showConfirmButton: true,
+        confirmButtonColor: '#58b953', 
+        confirmButtonText: 'Continue',
+        showCancelButton: true,
+        cancelButtonColor: '#ed3c3c',
+        cancelButtonText: 'Cancel',
+        html: '<p class="text-center">For the best experience, view the Digital Menu on a large device.</p>'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "https://exceptionalbean.swollenhippo.com/alexMenu/Menu/menu.html"
+            console.log('hello');
+        }
     })
 })
